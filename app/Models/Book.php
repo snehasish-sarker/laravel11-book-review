ba0cdae3ffb9bdbf513a75d44eba79f0dbc297f3
snehasish <snehasish.bgc@gmail.com>
2024-12-14 09:29:39 +0600

    feat: book list and search

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,40 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, $title) : Builder
+    {
+        return $query->where('title', 'LIKE', '%'.$title.'%');
+    }
+
+    public function scopePopular(Builder $query, $from=null, $to=null) : Builder
+    {
+        return $query->withCount(
+            ['reviews'=> fn(Builder $q) => $this->dateRangeFilter( $query, $from, $to)])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews) : Builder
+    {
+        return $query->having('review_count', '<=', $minReviews);
+    }
+
+    public function scopeHighestRated(Builder $query, $from=null, $to=null) : Builder
+    {
+        return $query->withAvg( ['reviews'=> fn(Builder $q) => $this->dateRangeFilter( $query, $from, $to)],
+            'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $query, $from=null, $to=null) : Builder
+    {
+        if($from && !$to){
+            $query->where('created_at', '>=', $from);
+        }elseif(!$from && $to){
+            $query->where('created_at', '<=', $to);
+        }elseif($from && $to){
+            $query->whereBetween('created_at', [$from, $to]);
+        };
     }
 }
---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 class="mb-10 text-2x1">Books</h1>
+
+    <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" placeholder="Search by Book title" class="input h-10" value="{{ request('title') }}">
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
+    </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+
+        @endphp
+    </div>
+
+    <ul>
+        @forelse ($books as $book)
+            <li class="mb-4">
+                <div class="book-item">
+                    <div
+                        class="flex flex-wrap items-center justify-between">
+                        <div class="w-full flex-grow sm:w-auto">
+                            <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                            <span class="book-author">by {{ $book->author }}</span>
+                        </div>
+                        <div>
+                            <div class="book-rating">
+                                {{ number_format($book->reviews_avg_rating, 1) }}
+                            </div>
+                            <div class="book-review-count">
+                                out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}G
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text">No books found</p>
+                    <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
+                </div>
+            </li>
+        @endforelse
+    </ul>
+
+@endsection
